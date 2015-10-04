b12af724ccc9160e6d4438b4231b2e71b6bebe2f
lautarodelosheros <lautarodelosheros@gmail.com>
2015-10-04 16:52:36 -0300

    Arreglo tipeos en Arrancar y unas comillas para que no rompa. Cambios en AFRAINIC

--- Arrancar.sh
+++ Arrancar.sh
@@ -4,27 +4,27 @@
 
 nombreScript=`basename "$0"`
 
-if [ "$BINDIR" = '' ]; then 
+if [ "$MAEDIR" = '' ]; then 
 	echo "Ambiente no inicializado"		
 	exit 1
 fi
 
 afrareciPid=`ps -e | grep '^.* AFRARECI$' | sed 's/ \?\([0-9]*\).*/\1/' | awk '{print $1}' | head -n 1`
 
-if [ afrareciPid != ""]; then
+if [ "$afrareciPid" != "" ]; then
 	echo "Error: AFRARECI se esta ejecutando con PID: ${afrareciPid}"
-	$GRUPO/GraLog.sh "$nombreScript" "El demonio ya se encuentra corriendo con PID: ${afrareciPid}"
+	$BINDIR/GraLog.sh "$nombreScript" "El demonio ya se encuentra corriendo con PID: ${afrareciPid}"
 	exit 0
 else
 	#aca tengo que ejecutar el comando
-	$BINDIR/AFRARECI.sh &
+	$BINDIR/AFRARECI &
 	afrareciPid=`ps -e | grep '^.* AFRARECI$' | sed 's/ \?\([0-9]*\).*/\1/' | awk '{print $1}' | head -n 1`
-	if [ afrareciPid != "" ]; then
+	if [ "$afrareciPid" != "" ]; then
 		echo "AFRARECI inicializado con PID: ${afrareciPid}"				
-		$GRUPO/Gralog.sh "$nombreScript" "Iniciando el demonio AFRARECI con el Process ID: ${afrareciPid}"
+		$BINDIR/GraLog.sh "$nombreScript" "Iniciando el demonio AFRARECI con el Process ID: ${afrareciPid}"
 		exit 0
 	else 
-		$GRUPO/Gralog.sh "$nombreScript" "Error al iniciar el proceso AFRARECI"
+		$BINDIR/GraLog.sh "$nombreScript" "Error al iniciar el proceso AFRARECI"
 	fi
 
 fi
